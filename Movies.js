3edcff1a1a75a085ad808f3542d39e6db5a8abf6
Octavio Hurtado Castañeda <o.hurtado@akron.com.mx>
2022-08-30 19:18:56 -0500

    agregue etiquetas

--- Movies.js
+++ Movies.js
@@ -6,11 +6,93 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const buscarURL = URLBASE + '/search/movie?' + API_KEY;
 
 
+
+const generos = [
+    {
+        "id":28,
+        "nombre": "Accion"
+    },
+    {
+        "id":12,
+        "nombre": "Aventura"
+    },
+    {
+        "id":35,
+        "nombre": "Comedia"
+    },
+    
+    {
+        "id":18,
+        "nombre": "Drama"
+    },
+    {
+        "id":14,
+        "nombre": "Fantasia"
+    },
+    {
+        "id":27,
+        "nombre": "Terror"
+    },
+    {
+        "id":10751,
+        "nombre": "Familia"
+    },
+    
+    ]
+//____________________
+
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const buscar = document.getElementById('Buscar')
+const etiquetasEL = document.getElementById('etiquetas') //nuevo
+
+var selectedGeneros = []
+setGenero();  
+function setGenero() {
+    etiquetasEL.innerHTML = '';
+    generos.forEach(genero => {
+        const t = document.createElement('div');
+        t.classList.add('etiqueta');
+        t.id=genero.id;
+        t.innerText = genero.nombre;
+        t.addEventListener('click', () => {
+            if(selectedGeneros.length == 0){
+                selectedGeneros.push(genero.id);
+            }else{
+                if(selectedGeneros.includes(genero.id)){
+                    selectedGeneros.forEach((id, idx) => {
+                        if(id == genero.id){
+                            selectedGeneros.splice(idx, 1);
+                        }
+                    })
+                }else{
+                    selectedGeneros.push(genero.id);
+                }
+            }
+            console.log(selectedGeneros)
+
+            getMovies(URLAPI + '&with_genres='+encodeURI(selectedGeneros.join(',')))
+            highlightSelection()
+        })
+        etiquetasEL.append(t);
+    })  
+        }
+        
+function highlightSelection() {
+    const etiquetas = document.querySelectorAll('.etiqueta');
+    etiquetas.forEach(etiqueta => {
+        etiqueta.classList.remove('highlight')
+    })
+    if(selectedGeneros.length !=0){
+        selectedGeneros.forEach(id => {
+            const hightlightedetiqueta = document.getElementById(id);
+            hightlightedetiqueta.classList.add('highlight');
+        })
+    }
+}
 
 
+//__________________________________________
 getMovies(URLAPI);
 
 function getMovies(url){
@@ -69,6 +151,10 @@ form.addEventListener('submit', (e) =>{
     }
 
 })
+
+function sayhello(){
+    console.log("hola como estas")
+}
 
 
 
